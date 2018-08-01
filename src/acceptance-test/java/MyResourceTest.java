8424b5d3b1c2e04b94b9fcf8b279dc5ed87c84d6
Will Porter <will.porter@unrulygroup.com>
2018-08-01 10:48:33 +0100

    test(MyResource): MyResource supplied method tested

--- src/acceptance-test/java/MyResourceTest.java
+++ src/acceptance-test/java/MyResourceTest.java
@@ -2,6 +2,7 @@ import com.willjsporter.MyResource;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 
 
 public class MyResourceTest {
@@ -9,6 +10,6 @@ public class MyResourceTest {
     @Test
     public void shouldReturnTheRightThing() {
         MyResource testMyResource = new MyResource();
-        assertThat(testMyResource.getClass(), is(class com.willjsporter.MyResource));
+        assertEquals(testMyResource.getIt(),"Got it!");
     }
 }
