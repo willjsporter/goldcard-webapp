59b320fe730ad0c4234cf8d58186b87c97695d0a
Will Porter <will.porter@unrulygroup.com>
2019-02-21 20:41:25 +0000

    refactor(local-memory-test):put setup in before hook

---
+++ src/test/java/com/willjsporter/notes/LocalMemoryStoreTest.java
@@ -0,0 +1,37 @@
+package com.willjsporter.notes;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class LocalMemoryStoreTest {
+
+    final int NOTE_0_ID = 0;
+    final String NOTE_0_MESSAGE = "note 0 message is this";
+    final int NOTE_1_ID = 1;
+    final String NOTE_1_MESSAGE = "note 1 message is this";
+
+    final Note testNote0 = new Note(NOTE_0_ID, NOTE_0_MESSAGE);
+    final Note testNote1 = new Note(NOTE_1_ID, NOTE_1_MESSAGE);
+
+    LocalMemoryStore localMemoryStore;
+
+    @Before
+    public void setup() {
+        localMemoryStore = new LocalMemoryStore();
+        localMemoryStore.save(testNote0);
+        localMemoryStore.save(testNote1);
+    }
+
+    @Test
+    public void shouldStoreNoteInLocalMemoryStore()  {
+        assertThat(localMemoryStore.getNote(NOTE_0_ID), is(testNote0));
+        assertThat(localMemoryStore.getNote(NOTE_1_ID), is(testNote1));
+    }
+
+}
